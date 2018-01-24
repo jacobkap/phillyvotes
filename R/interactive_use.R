c9fbd7075b940e0c8ac889b56ee9b094424a991a
jacobkap <jkkaplan6@gmail.com>
2018-01-24 17:06:16 -0500

    add a progress bar to the forloop.

--- R/interactive_use.R
+++ R/interactive_use.R
@@ -5,7 +5,7 @@
 philadelphia_votes <- function() {
   cat(paste("Please navigate to the folder with the voting files and select",
       "the files that you want. NOTE: selecting PDF files that are not voting",
-      "files will result in an error!",
+      "files may give wrong data!",
       "\n\nA window should have opened where you can select the files from. If you",
       "don't see it, try minimizing open windows on your screen (such as R).\n"))
   files <- tcltk::tk_choose.files(default = "", caption = "Select files",
@@ -13,14 +13,18 @@ philadelphia_votes <- function() {
   files <- files[grep("\\.PDF$", files, ignore.case = TRUE)]
 
   results <- data.frame()
-  message(paste("Collecting and organizing data now.\n\n",
-                "This process may take up to 15 minutes. Please be patient.",
+  message(paste0("\nCollecting and organizing data now.\n\n",
+                "This process may take up to 15 minutes. Please be patient. ",
                 "Thank you."))
-  for (file in files) {
-    results <- dplyr::bind_rows(results, philly_votes(file))
+
+  # Sets up progress bar
+  pb = txtProgressBar(min = 0, max = length(files), initial = 0)
+  for (i in seq_along(files)) {
+    results <- dplyr::bind_rows(results, philly_votes(files[i]))
+    setTxtProgressBar(pb, i)
   }
   rownames(results) <- 1:nrow(results)
   return(results)
 }
 
-# all_votes <- philadelphia_votes()
+ all_votes <- philadelphia_votes()
